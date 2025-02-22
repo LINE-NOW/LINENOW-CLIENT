e586f5d6abb1a3790c69a654fcb5a464fb883460
shimseohyun <shim020924@gmail.com>
2025-02-22 22:54:25 +0900

    [Chore] #3 - variant 오타 수정

--- packages/design-system/index.ts
+++ packages/design-system/index.ts
@@ -1,5 +1,9 @@
 export { default as LinenowProvider } from "./src/components/themeProvider/LinenowProvider";
 
+// styles
+export { type CustomTheme } from "./src/styles/theme";
+export { fonts } from "./src/styles/fonts";
+
 // components
 export { default as Label } from "./src/components/label/Label";
 export { default as Button } from "./src/components/button/Button";
