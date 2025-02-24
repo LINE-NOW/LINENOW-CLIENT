d20306f4616744f7dbd7cb3aa3d5aa874a249a2b
shimseohyun <shim020924@gmail.com>
2025-02-24 17:39:04 +0900

    [Feat] #5 - icon 컴포넌트 story 작성

---
+++ packages/design-system/src/components/icon/Icon.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Icon from "./Icon";
+import { IconList } from "./icons";
+import { IconsColorList } from "../../styles/theme";
+
+const meta = {
+  title: "Assets/Icon",
+  component: Icon,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    icon: {
+      control: { type: "select" },
+      options: IconList,
+    },
+
+    color: {
+      table: {
+        type: {
+          summary: "IconColorKey",
+          detail: "white\nblue\ngray\ngrayLight\ncss-color",
+        },
+      },
+      control: { type: "color" },
+      options: IconsColorList,
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// InputText의 기본 스토리
+export const Default: Story = {
+  args: { icon: "check", color: "blue", size: 24 },
+};
