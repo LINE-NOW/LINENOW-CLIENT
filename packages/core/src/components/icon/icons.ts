29706cc78e05e9f8d415a3525dbe1b6a4b9733db
shimseohyun <shim020924@gmail.com>
2025-03-11 21:49:46 +0900

    [Feat] #10 - icon 추가

--- packages/core/src/components/icon/icons.ts
+++ packages/core/src/components/icon/icons.ts
@@ -1,13 +1,19 @@
 import CheckIcon from "./assets/CheckIcon";
+import ClockIcon from "./assets/ClockIcon";
+import CopyIcon from "./assets/CopyIcon";
 import DownIcon from "./assets/DownIcon";
 import DropDownIcon from "./assets/DropDownIcon";
 import EditIcon from "./assets/EditIcon";
 import EyeCloseIcon from "./assets/EyeCloseIcon";
 import EyeOpenIcon from "./assets/EyeOpenIcon";
+import FestivalLocationIcon from "./assets/FestivalLocation";
 import KakaoIcon from "./assets/KakaoIcon";
 import LeftIcon from "./assets/LeftIcon";
+import ListIcon from "./assets/ListIcon";
 import LocationPinIcon from "./assets/LocationPinIcon";
 import LogoutIcon from "./assets/LogoutIcon";
+import MapIcon from "./assets/MapIcon";
+import MyLocationIcon from "./assets/MyLocationIcon";
 import PauseIcon from "./assets/PauseIcon";
 import PhoneIcon from "./assets/PhoneIcon";
 import PlayIcon from "./assets/PlayIcon";
@@ -43,6 +49,12 @@ export const icons = {
   sort: SortIcon,
   up: UpIcon,
   x: XIcon,
+  clock: ClockIcon,
+  copy: CopyIcon,
+  fetival_location: FestivalLocationIcon,
+  list: ListIcon,
+  map: MapIcon,
+  my_location: MyLocationIcon,
 };
 
 export const IconList = Object.keys(icons);
