74dca206c122d5646efdb54ea2dd2ecc3c707f6a
shimseohyun <shim020924@gmail.com>
2025-03-26 12:27:07 +0900

    [Feat] #25 - Notice Component

--- apps/service/src/pages/myWaiting/_components/MyWaitingList.tsx
+++ apps/service/src/pages/myWaiting/_components/MyWaitingList.tsx
@@ -1,6 +1,9 @@
 // components
 import { Flex } from "@linenow/core/components";
 import WaitingCard from "@components/waitingCard/WaitingCard";
+import Notice from "@components/notice/Notice";
+
+// dummy
 import { dummyFinishedWaitingsResponse, dummyWaitingsResponse } from "./dummy";
 
 interface MyWaitingListProps {
@@ -18,10 +21,9 @@ const MyWaitingList = (props: MyWaitingListProps) => {
     <Flex direction="column" gap="1.5rem">
       {/* 공지사항 */}
       {type === "waiting" && (
-        <div>
-          입장을 의사가 없을시 대기를 꼭 취소해주세요.
-          <br /> 노쇼가 확인 될 경우 라인나우 이용이 제한돼요.
-        </div>
+        <Notice
+          children={`입장을 의사가 없을시 대기를 꼭 취소해주세요.\n노쇼가 확인 될 경우 라인나우 이용이 제한돼요.`}
+        />
       )}
 
       {/* 대기 목록 */}
