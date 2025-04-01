2677967ebd849a4422946554fdfb818f5483f140
shimseohyun <shim020924@gmail.com>
2025-04-01 20:28:51 +0900

    [Feat] #25 - Waiting Button 추가

--- apps/service/src/components/waitingCard/WaitingCard.tsx
+++ apps/service/src/components/waitingCard/WaitingCard.tsx
@@ -1,76 +1,94 @@
 import * as S from "./WaitingCard.styled";
 
 import { Waiting } from "@interfaces/waiting";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-import BoothThumbnailCompact from "@components/booth/BoothThumbnailCompact";
 import { Button, Flex } from "@linenow/core/components";
+import BoothThumbnailCompact from "@components/booth/BoothThumbnailCompact";
+import EnteranceButton from "@components/button/EnteranceButton";
+import WaitingTeamsAheadButton from "@components/button/WaitingTeamsAhead";
 
 export interface WaitingCardProps
   extends Pick<
     Waiting,
-    "waitingID" | "waitingStatus" | "waitingTeamsAhead" | "booth"
+    | "waitingID"
+    | "waitingStatus"
+    | "waitingTeamsAhead"
+    | "booth"
+    | "arrivalarrivalDueTime"
   > {}
 
 interface Config {
   type?: "black" | "white";
   disabled?: boolean;
-  button: React.ComponentProps<typeof Button>;
+  button?: React.ReactNode;
 }
 
 const WaitingCard = (props: WaitingCardProps) => {
-  const { waitingID, waitingStatus, booth } = props;
+  const {
+    waitingID,
+    waitingStatus,
+    waitingTeamsAhead,
+    booth,
+    arrivalarrivalDueTime,
+  } = props;
 
   const getConfig = (): Config => {
     switch (waitingStatus) {
       case "ready_to_confirm":
         return {
-          button: { variant: "lime", children: "시간 내 입장해주세요" },
+          button: <EnteranceButton targetTime={arrivalarrivalDueTime} />,
         };
       case "waiting":
         return {
-          button: { variant: "blueLight", children: "내 앞으로 지금" },
+          button: (
+            <WaitingTeamsAheadButton waitingTeamsAhead={waitingTeamsAhead} />
+          ),
         };
       case "canceled":
         return {
-          button: {
-            variant: "grayLight",
-            children: "대기가 취소 되었어요",
-          },
+          disabled: true,
+          button: (
+            <Button
+              variant="grayLight"
+              children="대기가 취소 되었어요"
+              disabled={true}
+            />
+          ),
         };
       case "time_over_canceled":
         return {
-          button: {
-            variant: "grayLight",
-            children: "대기 시간이 초과 되었어요",
-          },
+          disabled: true,
+          button: (
+            <Button
+              variant="grayLight"
+              children="대기가 시간이 초과 되었어요"
+              disabled={true}
+            />
+          ),
         };
       default:
-        return {
-          button: {
-            children: "이런건 없음...",
-          },
-        };
+        return {};
     }
   };
 
-  const { disabled = false, type = "white", button: buttonProps } = getConfig();
+  const { disabled = false, type = "white", button } = getConfig();
 
+  const navigate = useNavigate();
+  const navigateWaitingDetail = () => {
+    navigate(`/waiting/${waitingID}`);
+  };
   return (
-    <Link to={`/waiting/${waitingID}`}>
-      <Flex
-        as="section"
-        direction="column"
-        gap="0.75rem"
-        css={[S.getWaitingCardStyle(type)]}
-      >
-        <BoothThumbnailCompact
-          css={[S.getBoothThumbnailStyle(disabled)]}
-          {...booth}
-        />
-        <Button {...buttonProps} />
-      </Flex>
-    </Link>
+    <Flex
+      as="section"
+      direction="column"
+      gap="0.75rem"
+      css={[S.getWaitingCardStyle(type, disabled)]}
+      onClick={disabled ? undefined : navigateWaitingDetail}
+    >
+      <BoothThumbnailCompact css={[S.getBoothThumbnailStyle()]} {...booth} />
+      {button}
+    </Flex>
   );
 };
 export default WaitingCard;
