82a5f577a54879103499446e037db78d72e3cb77
shimseohyun <shim020924@gmail.com>
2025-04-03 19:33:58 +0900

    [Feat] #29 - waiting status, booth status update

--- apps/service/src/lib/types/status.ts
+++ apps/service/src/lib/types/status.ts
@@ -1,10 +1,13 @@
 export type WaitingStatus =
-  | "check"
-  | "waiting"
-  | "ready_to_confirm"
-  | "confirmed"
-  | "arrived"
-  | "canceled"
-  | "time_over_canceled";
+  | "waiting" // 대기 중
+  | "entering" // 입장 중
+  | "canceled" // 대기 취소
+  | "time_over" // 대기 시간 초과
+  | "entered" // 입장 완료
+  | "not_waiting"; // 대기중이 아님
 
-export type BoothStatus = "not_started" | "operating" | "finished" | "paused";
+export type BoothStatus =
+  | "not_started" //부스 시작 전
+  | "operating" // 부스 운영 중
+  | "paused" // 부스 운영 중지
+  | "finished"; // 부스 운영 종료
