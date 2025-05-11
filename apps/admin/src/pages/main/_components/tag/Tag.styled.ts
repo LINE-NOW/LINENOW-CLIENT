87db4cab81690163cf99189d0f697743780e20ff
coldweek3 <ohchanju3@naver.com>
2025-05-12 03:21:20 +0900

    [Fix]#37-tag style dom 접근 emotion으로 처리

--- apps/admin/src/pages/main/_components/tag/Tag.styled.ts
+++ apps/admin/src/pages/main/_components/tag/Tag.styled.ts
@@ -1,25 +1,28 @@
+// Tag.styled.ts
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
-export const TagWrapper = styled.div<{ $isSelected?: boolean }>`
+interface TagWrapperProps {
+  isSelected?: boolean;
+}
+
+export const TagWrapper = styled.div<TagWrapperProps>`
   display: flex;
   flex-shrink: 0;
   align-items: center;
-
   padding: 0.5rem 0.75rem;
-
   border-radius: 1.375rem;
 
   background-color: ${({ theme }) => theme.backgroundColors.black};
-
   color: ${({ theme }) => theme.fontColors.gray};
   cursor: pointer;
 
-  ${({ $isSelected }) =>
-    $isSelected &&
-    `
-    box-shadow: 0 0 0 1px white;
-    color:white;
-  `}
+  ${({ isSelected }) =>
+    isSelected &&
+    css`
+      box-shadow: 0 0 0 1px white;
+      color: white;
+    `}
 `;
 
 export const TagImage = styled.img`
--- apps/admin/src/pages/main/_components/tag/TagList.tsx
+++ apps/admin/src/pages/main/_components/tag/TagList.tsx
@@ -15,29 +15,29 @@ const TagList = ({ selectedTag, onTagClick, ...props }: TagListProps) => {
     <TagListWrapper>
       <Tag
         label="전체보기"
-        $isSelected={selectedTag === "전체보기"}
+        isSelected={selectedTag === "전체보기"}
         onClick={() => onTagClick("전체보기")}
       />
       <Tag
         imageUrl="/images/tag_white.png"
         label={`대기 중 ${props.waiting}팀`}
-        $isSelected={selectedTag === "대기 중"}
+        isSelected={selectedTag === "대기 중"}
         onClick={() => onTagClick("대기 중")}
       />
       <Tag
         imageUrl="/images/tag_green.png"
         label={`호출 중 ${props.calling}팀`}
-        $isSelected={selectedTag === "호출 중"}
+        isSelected={selectedTag === "호출 중"}
         onClick={() => onTagClick("호출 중")}
       />
       <Tag
         label={`입장 완료 ${props.arrived}팀`}
-        $isSelected={selectedTag === "입장 완료"}
+        isSelected={selectedTag === "입장 완료"}
         onClick={() => onTagClick("입장 완료")}
       />
       <Tag
         label={`대기 취소 ${props.canceled}팀`}
-        $isSelected={selectedTag === "대기 취소"}
+        isSelected={selectedTag === "대기 취소"}
         onClick={() => onTagClick("대기 취소")}
       />
     </TagListWrapper>
