6edbe00ca92408820a6cb470866c7b6506e90f98
shimseohyun <shim020924@gmail.com>
2025-05-14 02:53:02 +0900

    [Feat] #43 - 회원가입 API 연결

--- apps/service/src/hooks/apis/user.tsx
+++ apps/service/src/hooks/apis/user.tsx
@@ -5,6 +5,7 @@ import { postRegistration } from "@apis/domains/user/postRegistration";
 import { postRegistrationMessage } from "@apis/domains/user/postRegistrationMessage";
 import { postWithdraw } from "@apis/domains/user/postWithdraw";
 import useAuth from "@hooks/useAuth";
+import { useToast } from "@linenow/core/hooks";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useGetBlackuser = () => {
@@ -43,6 +44,8 @@ export const usePostLogout = () => {
 };
 
 export const usePostRegistration = () => {
+  const { login } = useAuth();
+  const { presentToast } = useToast();
   type Prameter = {
     name: string;
     phonenumber: string;
@@ -54,22 +57,32 @@ export const usePostRegistration = () => {
     mutationKey: ["registration"],
     mutationFn: (params: Prameter) =>
       postRegistration({
-        userName: params.name,
-        userPhone: params.phonenumber,
+        user_name: params.name,
+        user_phone: params.phonenumber,
         sms_code: params.smsCode,
         user_password1: params.password,
         user_password2: params.passwordConfirm,
       }),
+    onSuccess: (response) => {
+      presentToast("회원가입을 성공했어요!");
+      if (response) login({ accessToken: response?.accessToken });
+    },
   });
 };
 
 export const usePostRegistrationMessage = () => {
+  const { presentToast } = useToast();
+  const presentCompletedSendingToast = () => {
+    presentToast("인증 번호가 전송되었어요!");
+  };
+
   return useMutation({
     mutationKey: ["registration_message"],
     mutationFn: (phonenumber: string) =>
       postRegistrationMessage({
         user_phone: phonenumber,
       }),
+    onSuccess: () => presentCompletedSendingToast(),
   });
 };
 
