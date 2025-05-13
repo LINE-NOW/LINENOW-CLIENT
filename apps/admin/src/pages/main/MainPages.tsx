fef2a39e3a85ad65140d3d38fe8bea46c7216756
coldweek3 <ohchanju3@naver.com>
2025-05-14 02:23:51 +0900

    [Feat]#20-admin websocket - waiting, canceld

--- apps/admin/src/pages/main/MainPages.tsx
+++ apps/admin/src/pages/main/MainPages.tsx
@@ -6,10 +6,14 @@ import TagList from "./_components/tag/TagList";
 import Spinner from "@components/spinner/Spinner";
 import { WaitingStatusParams } from "@linenow-types/status";
 import { useGetWaitingsCounts } from "@hooks/apis/boothManaging";
+import useWebSocket from "@hooks/useSocket";
+import { Waiting } from "@interfaces/waiting";
+import { transformGetWaitingResponse } from "@apis/domains/booth/_interfaces";
 
 const MainPage = () => {
   const { data: waitingsCounts } = useGetWaitingsCounts();
   const [selectedTag, setSelectedTag] = useState<string>("전체보기");
+  const [waitings, setWaitings] = useState<Waiting[]>([]);
 
   const getStatus = (tag: string): WaitingStatusParams => {
     switch (tag) {
@@ -26,14 +30,46 @@ const MainPage = () => {
     }
   };
 
+  //웹소켓 연결
+  const handleWebSocketMessage = (message: any) => {
+    const status = message?.data?.waiting_status;
+    console.log("status", status);
+    const updatedWaiting = transformGetWaitingResponse(message?.data);
+    console.log("update", updatedWaiting);
+
+    // 사용자가 대기 건 경우
+    setWaitings((prevWaitings) => {
+      if (status === "waiting") {
+        return [...prevWaitings, updatedWaiting];
+      }
+
+      // 대기 취소된 경우
+      if (status === "canceled") {
+        return prevWaitings.map((waiting) =>
+          waiting.waitingID === updatedWaiting.waitingID
+            ? { ...waiting, waitingStatus: "canceled" }
+            : waiting
+        );
+      }
+
+      return prevWaitings;
+    });
+  };
+
+  useWebSocket(handleWebSocketMessage);
+
   // API hooks
-  const { data: waitings, isLoading } = useGetWaitings(getStatus(selectedTag));
+  const { data, isLoading } = useGetWaitings(getStatus(selectedTag));
 
   const handleTagClick = (tag: string) => {
     setSelectedTag(tag);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (data) {
+      setWaitings(data);
+    }
+  }, [data]);
 
   if (isLoading) {
     return (
