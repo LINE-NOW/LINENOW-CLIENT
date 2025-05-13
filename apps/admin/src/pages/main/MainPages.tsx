58b5b26ea7e991552f582c3324f030cee263b111
coldweek3 <ohchanju3@naver.com>
2025-05-14 03:17:42 +0900

    [Feat]#20-admin websocket-대기 상태별 cnt

--- apps/admin/src/pages/main/MainPages.tsx
+++ apps/admin/src/pages/main/MainPages.tsx
@@ -10,10 +10,23 @@ import useWebSocket from "@hooks/useSocket";
 import { Waiting } from "@interfaces/waiting";
 import { transformGetWaitingResponse } from "@apis/domains/booth/_interfaces";
 
+interface BoothInfo {
+  waiting_team_cnt: number;
+  entering_team_cnt: number;
+  entered_team_cnt: number;
+  canceled_team_cnt: number;
+}
+
 const MainPage = () => {
   const { data: waitingsCounts } = useGetWaitingsCounts();
   const [selectedTag, setSelectedTag] = useState<string>("전체보기");
   const [waitings, setWaitings] = useState<Waiting[]>([]);
+  const [boothInfo, setBoothInfo] = useState<BoothInfo>({
+    waiting_team_cnt: 0,
+    entering_team_cnt: 0,
+    entered_team_cnt: 0,
+    canceled_team_cnt: 0,
+  });
 
   const getStatus = (tag: string): WaitingStatusParams => {
     switch (tag) {
@@ -54,6 +67,17 @@ const MainPage = () => {
 
       return prevWaitings;
     });
+
+    //부스 정보 소켓
+    const boothInfo = message?.data?.booth_info;
+    if (boothInfo) {
+      setBoothInfo((_) => ({
+        waiting_team_cnt: boothInfo.waiting_team_cnt,
+        entering_team_cnt: boothInfo.entering_team_cnt,
+        entered_team_cnt: boothInfo.entered_team_cnt,
+        canceled_team_cnt: boothInfo.canceled_team_cnt,
+      }));
+    }
   };
 
   useWebSocket(handleWebSocketMessage);
@@ -84,6 +108,10 @@ const MainPage = () => {
         selectedTag={selectedTag}
         onTagClick={handleTagClick}
         {...waitingsCounts}
+        waiting={boothInfo.waiting_team_cnt}
+        calling={boothInfo.entering_team_cnt}
+        arrived={boothInfo.entered_team_cnt}
+        canceled={boothInfo.canceled_team_cnt}
       />
 
       {waitings && waitings.length > 0 ? (
