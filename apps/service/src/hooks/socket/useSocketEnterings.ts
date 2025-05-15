272402448d364276272664d13c9c578e47673ebf
coldweek3 <ohchanju3@naver.com>
2025-05-16 06:58:57 +0900

    [Feat]#61-관리자가 취소시 취소 바텀 시트 소켓 연결

--- apps/service/src/hooks/socket/useSocketEnterings.ts
+++ apps/service/src/hooks/socket/useSocketEnterings.ts
@@ -4,12 +4,18 @@ import useWebSocket from "./useSocket";
 import { mapToWaiting } from "@utils/mapToWaiting";
 
 const useSocketEnterings = () => {
+  //TODO: 대기 취소 바텀시트 머지 후 주석 해제
+  // const { openEnteringWithFullData, openCancelBottomSheet } =
   const { openEnteringWithFullData } = useEnteringBottomSheet();
 
   const handleSocketMessage = useCallback(
     (data: any) => {
       const enteringWaitings = data?.data?.entering_waitings;
+      //TODO: 대기 취소 바텀시트 머지 후 주석 해제
+      // const canceled =
+      //   data?.data?.waiting_status === "canceled" ? data.data : null;
 
+      //입장 알림
       if (Array.isArray(enteringWaitings) && enteringWaitings.length > 0) {
         const validEnterings = enteringWaitings
           .filter((waiting) => waiting.waiting_status === "entering")
@@ -19,7 +25,17 @@ const useSocketEnterings = () => {
           openEnteringWithFullData(validEnterings);
         }
       }
+
+      //TODO: 대기 취소 바텀시트 머지 후 주석 해제
+      //   if (canceled) {
+      //     const mapped = mapToWaiting(canceled);
+      //     console.log("❌ 대기 취소 소켓 메시지:", mapped);
+      //     openCancelBottomSheet(mapped);
+      //   }
     },
+
+    //TODO: 대기 취소 바텀시트 머지 후 주석 해제
+    // [openEnteringWithFullData, openCancelBottomSheet]
     [openEnteringWithFullData]
   );
 
--- apps/service/src/hooks/useEntering.tsx
+++ apps/service/src/hooks/useEntering.tsx
@@ -3,6 +3,8 @@ import { useBottomSheet } from "@linenow/core/hooks";
 import { useGetWaitings } from "./apis/waiting";
 import useEnteringContent from "@components/bottomSheet/entering/useEnteringContent";
 import { Waiting } from "@interfaces/waiting";
+//TODO: 대기 취소 바텀시트 머지 후 주석 해제
+// import CancelBottomSheetContent from "@components/bottomSheet/cancel/CancelBottomSheet";
 
 const useEnteringBottomSheet = () => {
   const { data = [] } = useGetWaitings("waiting");
@@ -48,9 +50,22 @@ const useEnteringBottomSheet = () => {
     }
   }, [data]);
 
+  //TODO: 대기 취소 바텀시트 머지 후 주석 해제
+  // const openCancelBottomSheet = (waiting: Waiting) => {
+  //   openBottomSheet({
+  //     children: <CancelBottomSheetContent {...waiting} />,
+  //   });
+  // };
+
   const closeEntrace = () => closeBottomSheet();
 
-  return { openEntering, closeEntrace, openEnteringWithFullData };
+  return {
+    openEntering,
+    closeEntrace,
+    openEnteringWithFullData,
+    //TODO: 대기 취소 바텀시트 머지 후 주석 해제
+    // openCancelBottomSheet,
+  };
 };
 
 export default useEnteringBottomSheet;
