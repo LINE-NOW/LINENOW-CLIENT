eff3b1e24ce931cbabf03b451367945c96470b5e
coldweek3 <ohchanju3@naver.com>
2025-05-17 02:24:55 +0900

    [Fix]#71-관리자 이름 안뜨는거 수정

--- apps/admin/src/atoms/auth.ts
+++ apps/admin/src/atoms/auth.ts
@@ -32,9 +32,8 @@ export const authAtom = atom<AuthProps | null>(null);
 // export const authAtom = atom<AuthState>(undefined);
 // export const authAtom = atom<AuthState>(getLocalStorageTokens());
 
-
 // 마운트 시 localStorage에서 user 정보 가져옴
-// authAtom.onMount = (set) => {
-//   const tokens = getLocalStorageTokens();
-//   set(tokens);
-// };
+authAtom.onMount = (set) => {
+  const tokens = getLocalStorageTokens();
+  set(tokens);
+};
