522f75f0e52f319e842f1363dafac65e2e237dde
shimseohyun <shim020924@gmail.com>
2025-05-18 00:06:09 +0900

    [Feat] #64 - 회원가입 체크박스 추가

--- apps/service/src/components/bottomSheet/signup/AgreeBottomSheetContent.tsx
+++ apps/service/src/components/bottomSheet/signup/AgreeBottomSheetContent.tsx
@@ -1,18 +1,93 @@
-import { Button, ButtonLayout, Label } from "@linenow/core/components";
+import {
+  Button,
+  ButtonLayout,
+  Flex,
+  Icon,
+  Label,
+} from "@linenow/core/components";
 import * as S from "../BottomSheetContent.styled";
 import { useBottomSheet } from "@linenow/core/hooks";
+import { useState } from "react";
 
 interface AgreeBottomSheetContentProps {
   onClick: () => void;
 }
+
+interface Agree {
+  label: string;
+  url: string;
+}
+
+type CheckList = Record<keyof AgreeList, Agree>;
+
+interface AgreeList {
+  policy: boolean;
+  personal: boolean;
+  location: boolean;
+}
+
 const AgreeBottomSheetContent = (props: AgreeBottomSheetContentProps) => {
   const { onClick } = props;
+
+  const [checkList, setCheckList] = useState<AgreeList>({
+    policy: false,
+    personal: false,
+    location: false,
+  });
+
+  const isAllChecked = Object.keys(checkList).every(
+    (key) => checkList[key as keyof AgreeList]
+  );
+
   const { closeBottomSheet } = useBottomSheet();
   const handleAgreeButton = () => {
     onClick();
     closeBottomSheet();
   };
 
+  const agreeList: CheckList = {
+    policy: {
+      label: "라인나우 서비스 이용약관",
+      url: "https://thorn-freesia-96f.notion.site/1f5d72341c498025813dff9f58b0e412?pvs=4",
+    },
+    personal: {
+      label: "개인정보 수집·이용 동의서",
+      url: "https://thorn-freesia-96f.notion.site/1f5d72341c49807c8cd8cf249f3d04a9?pvs=4",
+    },
+    location: {
+      label: "위치정보 이용약관",
+      url: "https://thorn-freesia-96f.notion.site/1f5d72341c49806096fcfc7b694f0013?pvs=4",
+    },
+  };
+
+  const CheckLabel = (key: keyof AgreeList) => {
+    const isChecked = checkList[key];
+    const label = agreeList[key].label;
+    const url = agreeList[key].url;
+
+    return (
+      <Flex direction="row" gap="0.5rem">
+        <Flex
+          as={"button"}
+          onClick={() => {
+            setCheckList({ ...checkList, [key]: !checkList[key] });
+          }}
+          gap="0.25rem"
+        >
+          <Icon icon="check" color={isChecked ? "blue" : "grayLight"} />
+          <Label>{label}</Label>
+        </Flex>
+
+        <Icon
+          icon="right"
+          color="grayLight"
+          onClick={() => {
+            window.open(url, "_blank");
+          }}
+        />
+      </Flex>
+    );
+  };
   return (
     <S.BottomSheetContentWrapper>
       {/* 타이틀 */}
@@ -22,14 +97,20 @@ const AgreeBottomSheetContent = (props: AgreeBottomSheetContentProps) => {
         </Label>
       </S.BottomSheetTitleWrapper>
 
-      <div>
-        <div>개인정보 처리 방침</div>
-        <div>이용 약관 동의서</div>
-      </div>
+      <Flex direction="column" gap="0.25rem">
+        {CheckLabel("policy")}
+        {CheckLabel("personal")}
+        {CheckLabel("location")}
+      </Flex>
 
       {/* 버튼 */}
       <ButtonLayout colCount={1}>
-        <Button type="button" onClick={handleAgreeButton} variant="lime">
+        <Button
+          type="button"
+          onClick={handleAgreeButton}
+          variant="lime"
+          disabled={!isAllChecked}
+        >
           인증번호 전송하기
         </Button>
       </ButtonLayout>
