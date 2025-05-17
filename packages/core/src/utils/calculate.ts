0d41e2c9f2ad87b925c91dd22f5735b0fe00fa05
coldweek3 <ohchanju3@naver.com>
2025-05-18 02:51:34 +0900

    [Fix]#77-calculate  core로 이동

---
+++ packages/core/src/utils/calculate.ts
@@ -0,0 +1,15 @@
+const getEnteringTime = (confirmedAt: string) => {
+  const confirmedAtDate = new Date(confirmedAt);
+  if (isNaN(confirmedAtDate.getTime())) {
+    return "error";
+  }
+
+  const getTargetDate = (): string => {
+    confirmedAtDate.setMinutes(confirmedAtDate.getMinutes() + 10);
+    return confirmedAtDate.toISOString();
+  };
+
+  return getTargetDate();
+};
+
+export default getEnteringTime;
---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as getEnteringTime } from "./calculate";
