ad46f60ad1758cfba32d55586dd302459f7876e3
shimseohyun <shim020924@gmail.com>
2025-05-22 00:04:01 +0900

    [Fix] #100 - Build Error 해결

--- apps/service/src/pages/main/_hooks/useBoothViewState.ts
+++
@@ -1,8 +0,0 @@
-import { useAtomValue } from "jotai";
-import { isSelectedBoothAtom } from "@pages/main/_atom/selectedBooth";
-
-export const useBoothViewState = () => {
-  const selectedBoothID = useAtomValue(isSelectedBoothAtom);
-  const isBoothSelected = selectedBoothID !== null;
-  return { selectedBoothID, isBoothSelected };
-};
