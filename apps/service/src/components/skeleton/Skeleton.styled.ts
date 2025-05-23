453ceff1fa154b3da8fe53bae6732d4daa3ed156
shimseohyun <shim020924@gmail.com>
2025-05-23 12:12:43 +0900

    [Feat] #106 - Waiting Card Skeleton

--- apps/service/src/components/skeleton/Skeleton.styled.ts
+++ apps/service/src/components/skeleton/Skeleton.styled.ts
@@ -13,7 +13,14 @@ export const getSkeletonStyle = (width: string, height: string) => css`
   width: ${width};
   height: ${height};
   border-radius: 0.25rem;
-  background: linear-gradient(100deg, #ecf0f9 0%, #f2f6f9 20%, #ecf0f9 100%);
+  background: linear-gradient(
+    100deg,
+    #ecf0f9 0%,
+    #f2f6f9 20%,
+    #e2e6ef 50%,
+    #ecf0f9 100%
+  );
   background-size: 200% 100%;
   animation: ${shimmer} 1.5s infinite ease-in-out;
+  mix-blend-mode: multiply;
 `;
