be445c55482e530ac01141598a2fe676aa502aec
shimseohyun <shim020924@gmail.com>
2025-05-26 22:12:48 +0900

    [Fix] #124 - 네비게이션 온보딩 컴포넌트 화질 개선

--- apps/service/src/components/navigation/Navigation.styled.ts
+++ apps/service/src/components/navigation/Navigation.styled.ts
@@ -25,28 +25,3 @@ export const NavigationLabel = styled.h2`
   ${fonts.head2}
   color: ${({ theme }) => theme.fontColors.black};
 `;
-
-export const getFloatingOnBoardingStyle = () => css`
-  cursor: pointer;
-  position: absolute;
-  left: 3rem;
-  top: 28px;
-  transform: translateY(-50%);
-
-  /* 애니메이션 추가 */
-  animation: floating 2s ease-in-out infinite;
-  /* 애니메이션 정의 */
-  @keyframes floating {
-    0% {
-      opacity: 1;
-    }
-    50% {
-      left: 2.5rem;
-      opacity: 0.9;
-      transform: translateY(-50%) scale(0.98);
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-`;
