e72cbee8a304b9725b3ee11f2c21c8b059b631ec
shimseohyun <shim020924@gmail.com>
2025-05-26 21:52:08 +0900

    [Chore] #124 - Z index 수정

--- apps/service/src/components/navigation/Navigation.styled.ts
+++ apps/service/src/components/navigation/Navigation.styled.ts
@@ -11,7 +11,7 @@ export const NavigationWrapper = styled.header`
   transform: translate(-50%, 0%);
   top: 0;
   left: 50%;
-  /* z-index: 0; */
+  z-index: 2;
 
   width: 100%;
   max-width: 540px;
